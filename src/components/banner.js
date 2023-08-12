d897d08ccc726d92ccfe03309b882c53392c5c5b
Marisa Starkweather <marisastarkweather@gmail.com>
2023-08-12 04:23:53 -0400

    project complete

---
+++ src/components/banner.js
@@ -0,0 +1,11 @@
+//function that makes header tag
+
+const Banner = () => {
+        return (
+        <><header>
+                <h2>Orbit Report</h2>
+            </header><p>Click on the buttons to see the satellites in that orbit type</p></>
+        );
+};
+
+export default Banner;
